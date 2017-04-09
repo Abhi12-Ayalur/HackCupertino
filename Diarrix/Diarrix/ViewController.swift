82f58d4c1247916ed68562edae742498e71b6591
Ishaan Singhal <ishaansinghal@Ishaans-MacBook-Air.local>
2017-04-08 19:49:29 -0700

    firebase error

--- Diarrix/Diarrix/ViewController.swift
+++ Diarrix/Diarrix/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    let ref = FIRDatabase.database().reference(fromURL: "https://diarrix-aa799.firebaseio.com/")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var signUpFirstName: UITextField!
+    @IBOutlet weak var signUpLastName: UITextField!
+    @IBOutlet weak var signUpEmail: UITextField!
+    @IBOutlet weak var signUpPhoneNumber: UITextField!
+    @IBOutlet weak var signUpPassword: UITextField!
+    
+    
+    
+    @IBAction func signUpUser(_ sender: Any) {
+        if signUpEmail.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            FIRAuth.auth()?.createUser(withEmail: signUpEmail.text!, password: signUpPassword.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                    
+                    let user = FIRAuth.auth()?.currentUser
+                    let uid = user?.uid
+                    let usersRef = self.ref.child("users").child(uid!)
+                    let values = ["firstName": self.signUpFirstName.text!, "lastName": self.signUpLastName.text!, "Email": self.signUpEmail.text!, "phoneNumber": self.signUpPhoneNumber.text!, "Password": self.signUpPassword.text!]
+                    usersRef.ref.updateChildValues(values, withCompletionBlock: { (error, ref)
+                        in
+                        if error != nil{
+                            print(error)
+                            return
+                        }
+                    })
+                    
+                    
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
 }
+    
+
 
