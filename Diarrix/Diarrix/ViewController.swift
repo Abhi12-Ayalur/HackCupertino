4cce959c53cc987fbdcd0d08fc3d3c1de66ac969
Ishaan Singhal <ishaansinghal00@gmail.com>
2017-04-11 23:29:01 -0700

    added UI elements

--- Diarrix/Diarrix/ViewController.swift
+++ Diarrix/Diarrix/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var signUpPhoneNumber: UITextField!
     @IBOutlet weak var signUpPassword: UITextField!
     
+    @IBOutlet weak var signUpConfirmPassword: UITextField!
     
     
     @IBAction func signUpUser(_ sender: Any) {
@@ -45,7 +46,17 @@ class ViewController: UIViewController {
             
             present(alertController, animated: true, completion: nil)
             
-        } else {
+        }
+        else if  signUpPassword.text! != signUpConfirmPassword.text!{
+            let alertController = UIAlertController(title: "Error", message: "Passwords do not match", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        }
+        else {
             FIRAuth.auth()?.createUser(withEmail: signUpEmail.text!, password: signUpPassword.text!) { (user, error) in
                 
                 if error == nil {
